c781a724a204fd6126c471bf3b419e0d8123bacf
Olha75 <137830839+Olha75@users.noreply.github.com>
2024-02-05 21:37:09 +0200

    modal_useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,106 +7,86 @@ import { searchImages } from '../api/posts';
 import css from './app.module.css';
 import ImageGallery from './ImageGallery/ImageGallery';
 
-
 const App = () => {
   const [search, setSearch] = useState('');
   const [items, setItems] = useState([]);
-  const [loading, setLoading] = usestate(false);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [page, setPage] = useState(1);
   const [modalOpen, setModalOpen] = useState(false);
   const [itemDetails, setItemDetails] = useState({});
   const [totalHits, setTotalHits] = useState(0);
 
-
-
-const handleSearch = ({ search }) => {
-  setSearch(search);
-  setItems([]);
-  setPage(1);
-
-  };
-
-  loadMore = () => {
-    setPage(prevPage => prevPage + 1 );
-  };
-
-  showModal = largeImageURL => {
-    this.setState({
-      modalOpen: true,
-      itemDetails: {
-        largeImageURL,
-      },
-    });
-  };
-
-  closeModal = () => {
-    this.setState({
-      modalOpen: false,
-      itemDetails: {},
-    });
-  };
-
-
   useEffect(() => {
     const getApi = async () => {
       try {
         setLoading(true);
         const { data } = await searchImages(search, page);
-        setSearch(data?.length ? data : []);
-        setTotalHits(data.totalHits);
-        
-      } else {
-        alert('Вибачте, сталася помилка, спробуйте ще.');
-      }
-    catch (error) {
+        setSearch(prevSearch =>
+          data?.length ? [...prevSearch, ...data] : prevSearch
+        );
+      } catch (error) {
         setError(error.message);
       } finally {
         setLoading(false);
       }
-    } getApi()
-  },[])
+    };
 
+    if (search) {
+      getApi();
+    }
+  }, [search, page]);
 
-  
+  const handleSearch = ({ search }) => {
+    setSearch(search);
+    setItems([]);
+    setPage(1);
+  };
 
-  render() {
-    const { handleSearch, loadMore, showModal, closeModal } = this;
-    const { items, loading, error, modalOpen, itemDetails, totalHits } =
-      this.state;
-    const isItems = Boolean(items.length);
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
 
-    // console.log('App - items:', items);
+  const showModal = ({ largeImageURL }) => {
+    setModalOpen(true);
+    setItemDetails({ largeImageURL });
+  };
 
-    return (
-      <div
-        style={{
-          // height: '100vh',
-          // display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Searchbar onSubmit={handleSearch} />
-        {error && <p className={css.error}>помилка завантаження</p>}
-        {loading && <Loader />}
-        {/* <SearchBar searchImages={searchImages} /> */}
-        {isItems && <ImageGallery items={items} showModal={showModal} />}
-        {isItems && items.length < totalHits ? (
-          <div className={css.buttonLM}>
-            <Button onClick={loadMore} type="button">
-              Продовжити
-            </Button>
-          </div>
-        ) : null}
-        {modalOpen && (
-          <Modal largeImageURL={itemDetails.largeImageURL} close={closeModal} />
-        )}
-      </div>
-    );
-  }
-}
+  const closeModal = () => {
+    setModalOpen(false);
+    setItemDetails({});
+  };
+
+  const isItems = Boolean(items.length);
+
+  return (
+    <div
+      style={{
+        ustifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Searchbar onSubmit={handleSearch} />
+      {error && (
+        <p className={css.error}>Вибачте, сталася помилка, спробуйте ще</p>
+      )}
+      {loading && <Loader />}
+      {/* <SearchBar searchImages={searchImages} /> */}
+      {isItems && <ImageGallery items={items} showModal={showModal} />}
+      {isItems && items.length < totalHits ? (
+        <div className={css.buttonLM}>
+          <Button onClick={loadMore} type="button">
+            Продовжити
+          </Button>
+        </div>
+      ) : null}
+      {modalOpen && (
+        <Modal largeImageURL={itemDetails.largeImageURL} close={closeModal} />
+      )}
+    </div>
+  );
+};
 
 export default App;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,37 +1,28 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import css from './modal.module.css';
 
 const modalRoot = document.getElementById('modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.closeModal);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.closeModal);
-  }
-
-  closeModal = ({ target, currentTarget, code }) => {
+const Modal = ({ close, children }) => {
+  const closeModal = ({ target, currentTarget, code }) => {
     if (target === currentTarget || code === 'Escape') {
-      this.props.close();
+      close();
     }
   };
+  useEffect(() => {
+    document.addEventListener('keydown', closeModal);
+    return () => document.addEventListener('keydown', closeModal);
+  }, []);
 
-  render() {
-    const { closeModal } = this;
-    const { largeImageURL } = this.props;
-
-    return createPortal(
-      <div onClick={closeModal} className={css.overlay}>
-        <div className={`${css.modal} ${css.neonBorder}`}>
-          <img src={largeImageURL} alt="" />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <div onClick={closeModal} className={css.overlay}>
+      <div className={`${css.modal} ${css.neonBorder}`}>
+        <img src={largeImageURL} alt="" />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
 
 export default Modal;
